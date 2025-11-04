49546b0ba5d5668a52ee3fa6af87375ec7113f44
Toon-van-Berkel <toonvanberkel203@gmail.com>
2025-11-04 14:33:11 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const dev = process.argv.includes('dev');
 const repo = 'CMGT-CLE1-Team4-Simulation';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      fallback: '200.html'
+      fallback: '404.html'
     }),
     paths: {
       base: dev ? '' : `/${repo}`
